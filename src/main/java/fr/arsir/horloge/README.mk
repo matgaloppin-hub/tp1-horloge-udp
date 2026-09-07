90fef5386e3366473915a8d4e0c0c12103ab22f8
Mattis Galoppin <mattis.galoppin@sncf.fr>
2026-09-07 14:13:39 +0200

    readme

---
+++ src/main/java/fr/arsir/horloge/README.mk
@@ -0,0 +1,3 @@
+###exercice 2 : Serveur Horloge UDP
+Justification UDP :
+UDP est utilisé car il est plus rapide et permet un échange direct de datagrammes, ce qui est adapté à la mesure des temps de transmission entre le client et le serveur.
